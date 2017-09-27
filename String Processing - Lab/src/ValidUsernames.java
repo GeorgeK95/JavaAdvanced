e61c64981e5a6848e16db62f6ad73c4b35f62e0d
GeorgeK95 <george_it@abv.bg>
2017-09-27 21:32:35 +0300

    Fixed task 7.

--- String Processing - Lab/src/ValidUsernames.java
+++ String Processing - Lab/src/ValidUsernames.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,20 +9,36 @@ import java.util.regex.Pattern;
  */
 public class ValidUsernames {
     public static void main(String[] args) {
+        List<String> validUsernames = getValidUsernames();
+        print(validUsernames);
+    }
+
+    private static List<String> getValidUsernames() {
+        List<String> validUsernames = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
         String pattern = "^[A-Za-z\\d_-]{3,16}$";
         Pattern r = Pattern.compile(pattern);
-
         while (!line.equals("END")) {
             Matcher m = r.matcher(line);
             if (m.find()) {
-                System.out.println("valid");
+                validUsernames.add("valid");
             } else {
-                System.out.println("invalid");
+                validUsernames.add("invalid");
             }
             line = in.nextLine();
         }
+
+        return validUsernames;
     }
+
+    private static void print(List<String> validUsernames) {
+        if (validUsernames.contains("valid")) {
+            for (String name : validUsernames) {
+                System.out.println(name);
+            }
+        }
+    }
+
 
 }
